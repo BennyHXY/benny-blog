ee40614ced136aa10c7134918b0e9a7525e418c5
BennyHXY <56120893+BennyHXY@users.noreply.github.com>
2026-04-21 06:18:49 +0000

    Deploying to gh-pages from @ BennyHXY/benny-blog@8f39c9147325d52b882204696604a701991a93ed 🚀

---
+++ assets/echarts1.html-Dxjap0w4.js
@@ -0,0 +1,106 @@
+import{_ as u}from"./plugin-vue_export-helper-DlAUqK2U.js";import{c as r,a as n,d as a,w as e,r as p,o as t,b as s}from"./app-DvoVaV-B.js";const d={};function m(b,l){const i=p("ECharts"),c=p("VPPreview");return t(),r("div",null,[l[2]||(l[2]=n("h1",{id:"echarts1",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#echarts1"},[n("span",null,"ECharts1")])],-1)),l[3]||(l[3]=n("h2",{id:"柱状图",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#柱状图"},[n("span",null,"柱状图")])],-1)),a(c,{title:"展开查看代码"},{content:e(()=>[a(i,{config:"eJyNUl1rE0EUfe+vuOxTg4ttRQOmqJTggyDoi76oD5NkkixudsPspCZIIa2NNC1JhbaQtmCIppKiVfEpTRP9Mzu72af+BW/2I1lsbAvL3rn33Htm5pxJ6prBQc9zRdfgHrydAZibA+ukLdrrZrcHD+NZwrgB4vD3sNVxKjV78B17uK6rCb0YcwcAjKz+JgacFajs5mlKeIHRAAbIEfZ6kl0cAFgJFinCyXOFInxZOwCjJPVEU0sxSBPVuMjDqMH18Bku3dUgy3TJeJQjmetNuMH9qTRDtRQOudlIGK7kg7S4VFSMgFAjOSSXzO7Z8MvqsPzOXjuVPDZeyo+QJOE0o7OSXx0pEYMX/saSqG2KH31zsCNOGmKtI/Y7fh9iyIlV+2Ddqh6LSkVsb5rdevzpM6wM/3y06keh1kHdHNTMXltsV6cw2Z9X7cPGFMD52nDKTbu/I97/GsPn/Y04SWbpeb866RzuH1hbzX8pXnmBoCCPSYKqE5kVjVO2TLAyH3CkdZYjHMvobkFLuq9zFlsKNBJyh1F8ZRq49Zsq1TI8C/chOsYBHvigoSpJOjsvQzQCN0B6qUkYwlA0EpqKedDi/ywv+a4GjqKXVvPI+tay9n5KcuCmyyF5EwZlCsURz8zgBn5jgrCxeD6jqO05rVOnXLW2jp1PjTHsP4koXkSGWzLclmEBwx1fXP+kV/PbZ33xYUP0du3dzhT+BaR1P1TsLn5helyvLM78BbM6Y+k=",title:"%E7%9C%9F%E9%A2%98%E5%88%86%E5%B8%83",type:"js"})]),code:e(()=>[...l[0]||(l[0]=[n("div",{class:"language- line-numbers-mode","data-highlighter":"shiki","data-ext":"",style:{"--shiki-light":"#383A42","--shiki-dark":"#abb2bf","--shiki-light-bg":"#FAFAFA","--shiki-dark-bg":"#282c34"}},[n("pre",{class:"shiki shiki-themes one-light one-dark-pro vp-code"},[n("code",{class:"language-"},[n("span",{class:"line"},[n("span",null,"::: echarts 真题分布")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span",null,"```js")]),s(`
+`),n("span",{class:"line"},[n("span",null,"const option = {")]),s(`
+`),n("span",{class:"line"},[n("span",null,"  // 此处为 ECharts 图表配置")]),s(`
+`),n("span",{class:"line"},[n("span",null,"  toolbox: {")]),s(`
+`),n("span",{class:"line"},[n("span",null,"    show: true,")]),s(`
+`),n("span",{class:"line"},[n("span",null,"    feature: {")]),s(`
+`),n("span",{class:"line"},[n("span",null,"      mark: {")]),s(`
+`),n("span",{class:"line"},[n("span",null,"        show: true,")]),s(`
+`),n("span",{class:"line"},[n("span",null,"      },")]),s(`
+`),n("span",{class:"line"},[n("span",null,"      dataView: {")]),s(`
+`),n("span",{class:"line"},[n("span",null,"        show: true,")]),s(`
+`),n("span",{class:"line"},[n("span",null,"        readOnly: false,")]),s(`
+`),n("span",{class:"line"},[n("span",null,"      },")]),s(`
+`),n("span",{class:"line"},[n("span",null,"      restore: {")]),s(`
+`),n("span",{class:"line"},[n("span",null,"        show: true,")]),s(`
+`),n("span",{class:"line"},[n("span",null,"      },")]),s(`
+`),n("span",{class:"line"},[n("span",null,"      saveAsImage: {")]),s(`
+`),n("span",{class:"line"},[n("span",null,"        show: true,")]),s(`
+`),n("span",{class:"line"},[n("span",null,"      },")]),s(`
+`),n("span",{class:"line"},[n("span",null,"    },")]),s(`
+`),n("span",{class:"line"},[n("span",null,"  },")]),s(`
+`),n("span",{class:"line"},[n("span",null,"  legend: {},")]),s(`
+`),n("span",{class:"line"},[n("span",null,"  tooltip: {},")]),s(`
+`),n("span",{class:"line"},[n("span",null,"  xAxis: {")]),s(`
+`),n("span",{class:"line"},[n("span",null,'    name: "主要考点",')]),s(`
+`),n("span",{class:"line"},[n("span",null,'    type: "category",')]),s(`
+`),n("span",{class:"line"},[n("span",null,"    data: [")]),s(`
+`),n("span",{class:"line"},[n("span",null,'      "半导体存储器",')]),s(`
+`),n("span",{class:"line"},[n("span",null,'      "主存的扩充及与CPU的连接",')]),s(`
+`),n("span",{class:"line"},[n("span",null,'      "低位交叉存储器",')]),s(`
+`),n("span",{class:"line"},[n("span",null,'      "磁盘存储器",')]),s(`
+`),n("span",{class:"line"},[n("span",null,'      "高速缓冲存储器（Cache）",')]),s(`
+`),n("span",{class:"line"},[n("span",null,'      "虚拟存储器",')]),s(`
+`),n("span",{class:"line"},[n("span",null,"    ],")]),s(`
+`),n("span",{class:"line"},[n("span",null,"    axisLabel: {")]),s(`
+`),n("span",{class:"line"},[n("span",null,"      interval: 0,")]),s(`
+`),n("span",{class:"line"},[n("span",null,"      formatter: function (value) {")]),s(`
+`),n("span",{class:"line"},[n("span",null,"        return value.length > 6")]),s(`
+`),n("span",{class:"line"},[n("span",null,'          ? value.slice(0, 6) + "\\n" + value.slice(6)')]),s(`
+`),n("span",{class:"line"},[n("span",null,"          : value;")]),s(`
+`),n("span",{class:"line"},[n("span",null,"      },")]),s(`
+`),n("span",{class:"line"},[n("span",null,"    },")]),s(`
+`),n("span",{class:"line"},[n("span",null,"  },")]),s(`
+`),n("span",{class:"line"},[n("span",null,'  yAxis: { name: "考查次数", type: "value" },')]),s(`
+`),n("span",{class:"line"},[n("span",null,"  series: [")]),s(`
+`),n("span",{class:"line"},[n("span",null,"    {")]),s(`
+`),n("span",{class:"line"},[n("span",null,'      type: "bar",')]),s(`
+`),n("span",{class:"line"},[n("span",null,'      name: "单项选择题",')]),s(`
+`),n("span",{class:"line"},[n("span",null,"      data: [6, 6, 2, 4, 12, 5],")]),s(`
+`),n("span",{class:"line"},[n("span",null,"    },")]),s(`
+`),n("span",{class:"line"},[n("span",null,"    {")]),s(`
+`),n("span",{class:"line"},[n("span",null,'      type: "bar",')]),s(`
+`),n("span",{class:"line"},[n("span",null,'      name: "综合应用题",')]),s(`
+`),n("span",{class:"line"},[n("span",null,"      data: [1, 1, 1, 0, 9, 9],")]),s(`
+`),n("span",{class:"line"},[n("span",null,"    },")]),s(`
+`),n("span",{class:"line"},[n("span",null,"  ],")]),s(`
+`),n("span",{class:"line"},[n("span",null,"};")]),s(`
+`),n("span",{class:"line"},[n("span",null,"```")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span",null,":::")])])]),n("div",{class:"line-numbers","aria-hidden":"true",style:{"counter-reset":"line-number 0"}},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1)])]),_:1}),a(c,{title:"Demo"},{content:e(()=>[a(i,{config:"eJyNUE1Lw0AQvfdXDDkHWit6iHgo4sGTNy/Fw9Zu22CaLbtTTZGCB4ugiB4EhYpSjVCs1aMSsL+mbZJ/4WZJUrFQetiP92bevJk5YLZAYA00mQ2bcJIByGZhOnQn7tn424PtrRrhKGDSHQW9fti58n8+ZA4yZpWYYygBgKixYwOQN6mucIUSbHKahAHqhB/O0LwAoJ18ygTJnklleFE6AKekvGtbLQMqxBLzdTgVyP72sNBVkCNaEDt1Ul1OoR51oYlWqkHqoAFaurXOa3jX911vPLrUZgq5PDQbUqOgRavULicVovENKGrh7Wl4fq3tpyqn4Jjif1bQew96A00HzR9dTD+7MQi7b77nxSB4cSfDG0UP7oOvp/BR9iLp54eInhm0YgMFBOUmlaio7JKF2KQuR016S1aCrUbElghPqbi/NR3yOR1W13VYkW908jnlGHvKf3sj8wtBFMjm",title:"Title",type:"js"})]),code:e(()=>[...l[1]||(l[1]=[n("div",{class:"language- line-numbers-mode","data-highlighter":"shiki","data-ext":"",style:{"--shiki-light":"#383A42","--shiki-dark":"#abb2bf","--shiki-light-bg":"#FAFAFA","--shiki-dark-bg":"#282c34"}},[n("pre",{class:"shiki shiki-themes one-light one-dark-pro vp-code"},[n("code",{class:"language-"},[n("span",{class:"line"},[n("span",null,"::: echarts Title")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span",null,"```js")]),s(`
+`),n("span",{class:"line"},[n("span",null,"const option = {")]),s(`
+`),n("span",{class:"line"},[n("span",null,"  // 此处为 ECharts 图表配置")]),s(`
+`),n("span",{class:"line"},[n("span",null,"  toolbox: {")]),s(`
+`),n("span",{class:"line"},[n("span",null,"    show: true,")]),s(`
+`),n("span",{class:"line"},[n("span",null,"    feature: {")]),s(`
+`),n("span",{class:"line"},[n("span",null,"      mark: {")]),s(`
+`),n("span",{class:"line"},[n("span",null,"        show: true,")]),s(`
+`),n("span",{class:"line"},[n("span",null,"      },")]),s(`
+`),n("span",{class:"line"},[n("span",null,"      dataView: {")]),s(`
+`),n("span",{class:"line"},[n("span",null,"        show: true,")]),s(`
+`),n("span",{class:"line"},[n("span",null,"        readOnly: false,")]),s(`
+`),n("span",{class:"line"},[n("span",null,"      },")]),s(`
+`),n("span",{class:"line"},[n("span",null,"      restore: {")]),s(`
+`),n("span",{class:"line"},[n("span",null,"        show: true,")]),s(`
+`),n("span",{class:"line"},[n("span",null,"      },")]),s(`
+`),n("span",{class:"line"},[n("span",null,"      saveAsImage: {")]),s(`
+`),n("span",{class:"line"},[n("span",null,"        show: true,")]),s(`
+`),n("span",{class:"line"},[n("span",null,"      },")]),s(`
+`),n("span",{class:"line"},[n("span",null,"    },")]),s(`
+`),n("span",{class:"line"},[n("span",null,"  },")]),s(`
+`),n("span",{class:"line"},[n("span",null,"  title: {")]),s(`
+`),n("span",{class:"line"},[n("span",null,'    text: "ECharts 入门示例",')]),s(`
+`),n("span",{class:"line"},[n("span",null,"  },")]),s(`
+`),n("span",{class:"line"},[n("span",null,"  tooltip: {},")]),s(`
+`),n("span",{class:"line"},[n("span",null,"  legend: {")]),s(`
+`),n("span",{class:"line"},[n("span",null,'    data: ["销量"],')]),s(`
+`),n("span",{class:"line"},[n("span",null,"  },")]),s(`
+`),n("span",{class:"line"},[n("span",null,"  xAxis: {")]),s(`
+`),n("span",{class:"line"},[n("span",null,'    data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],')]),s(`
+`),n("span",{class:"line"},[n("span",null,"  },")]),s(`
+`),n("span",{class:"line"},[n("span",null,"  yAxis: {},")]),s(`
+`),n("span",{class:"line"},[n("span",null,"  series: [")]),s(`
+`),n("span",{class:"line"},[n("span",null,"    {")]),s(`
+`),n("span",{class:"line"},[n("span",null,'      name: "销量",')]),s(`
+`),n("span",{class:"line"},[n("span",null,'      type: "bar",')]),s(`
+`),n("span",{class:"line"},[n("span",null,"      data: [5, 20, 36, 10, 10, 20],")]),s(`
+`),n("span",{class:"line"},[n("span",null,"    },")]),s(`
+`),n("span",{class:"line"},[n("span",null,"  ],")]),s(`
+`),n("span",{class:"line"},[n("span",null,"};")]),s(`
+`),n("span",{class:"line"},[n("span",null,"```")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span",null,":::")])])]),n("div",{class:"line-numbers","aria-hidden":"true",style:{"counter-reset":"line-number 0"}},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1)])]),_:1})])}const h=u(d,[["render",m]]),g=JSON.parse('{"path":"/zh/posts/playground/echarts1.html","title":"ECharts1","lang":"zh-CN","frontmatter":{"title":"ECharts1","icon":"pen-to-square","date":"2026-04-05T00:00:00.000Z","category":["数据统计","图表"],"tag":["红","小","圆"],"description":"ECharts1 柱状图","head":[["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"ECharts1\\",\\"image\\":[\\"\\"],\\"datePublished\\":\\"2026-04-05T00:00:00.000Z\\",\\"dateModified\\":\\"2026-04-05T06:54:38.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"BennyHXY\\",\\"url\\":\\"https://mister-hope.com\\"}]}"],["meta",{"property":"og:url","content":"https://mister-hope.github.io/benny-blog/zh/posts/playground/echarts1.html"}],["meta",{"property":"og:site_name","content":"胡小言的博客"}],["meta",{"property":"og:title","content":"ECharts1"}],["meta",{"property":"og:description","content":"ECharts1 柱状图"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2026-04-05T06:54:38.000Z"}],["meta",{"property":"article:tag","content":"圆"}],["meta",{"property":"article:tag","content":"小"}],["meta",{"property":"article:tag","content":"红"}],["meta",{"property":"article:published_time","content":"2026-04-05T00:00:00.000Z"}],["meta",{"property":"article:modified_time","content":"2026-04-05T06:54:38.000Z"}]]},"git":{"createdTime":1775372078000,"updatedTime":1775372078000,"contributors":[{"name":"BennyHXY","username":"BennyHXY","email":"1470011518@qq.com","commits":1,"url":"https://github.com/BennyHXY"}]},"readingTime":{"minutes":0.84,"words":251},"filePathRelative":"zh/posts/playground/echarts1.md","excerpt":"\\n<h2>柱状图</h2>\\n\\n\\n","autoDesc":true}');export{h as comp,g as data};
